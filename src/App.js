321666bc07f4bfa0b588067a310a2d11520c9376
Suat-17 <yargicisuat@gmail.com>
2023-02-01 14:05:06 +0300

    chore: general settings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import { useTranslation } from 'react-i18next'; 
 const App = () => {
+  const { t, i18n } = useTranslation();
   return (
     <div>
-
+{t("welcome")}
+      <button
       
+        onClick={() => i18n.changeLanguage('en')}
+      >
+        {t("change-locale")}
+      </button>
     </div>
   )
 }
