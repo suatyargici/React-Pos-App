17451451da4da60a91a4c3db798a905255e6bb1a
Suat-17 <yargicisuat@gmail.com>
2023-02-01 15:55:51 +0300

    chore: added header responsive

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'; 
+import React from "react";
+import { useTranslation } from "react-i18next";
+import Header from "./components/header/Header";
+
 const App = () => {
   const { t, i18n } = useTranslation();
   return (
     <div>
-{t("welcome")}
-      <button
-      
-        onClick={() => i18n.changeLanguage('en')}
-      >
+      <Header/>
+      {t("welcome")}
+      <button onClick={() => i18n.changeLanguage("en")}>
         {t("change-locale")}
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
