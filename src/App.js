b2ab3fe70da612f51ffd8254b03d5dda7de6fd8c
Suat-17 <yargicisuat@gmail.com>
2023-02-02 11:20:32 +0300

    feat: fixed categories and product

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
+import CartTotals from "./components/cart/cartTotals";
+import Categories from "./components/categories/Categories";
 import Header from "./components/header/Header";
+import Products from "./components/products/Products";
 
 const App = () => {
-  const { t, i18n } = useTranslation();
   return (
-    <div>
-      <Header/>
-      {t("welcome")}
-      <button onClick={() => i18n.changeLanguage("en")}>
-        {t("change-locale")}
-      </button>
+    <>
+    <Header />
+    <div className="home px-6 flex justify-between gap-10">
+        <div className="categories  overflow-auto max-h-[calc(100vh_-_112px)] pb-64">
+          <Categories />
+        </div>
+        <div className="products flex-[8]">
+          <Products/>
+      </div>
+      <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
+          <CartTotals />
+      </div>
     </div>
+  </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        "card": "repeat(auto-fill, minmax(150px, 1fr))"
+      },
+    },
   },
   plugins: [],
-}
+};
