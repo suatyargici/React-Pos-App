186e4103ce5dfb633b13555187c515fcaa940685
Suat-17 <yargicisuat@gmail.com>
2023-02-02 11:44:47 +0300

    feat: added cart totals

--- src/components/cart/cartTotals.jsx
+++ src/components/cart/cartTotals.jsx
@@ -1,15 +1,160 @@
 import { Button } from "antd";
-import { ClearOutlined } from '@ant-design/icons';
+import {
+  ClearOutlined,
+  PlusCircleOutlined,
+  MinusCircleOutlined,
+} from "@ant-design/icons";
 
 const CartTotals = () => {
   return (
-    <div className="cart h-full max-h-[calc(100vh_-_90px)] flex flex-col">
-      <h2 className="bg-blue-600 text-center py-4 text-white font-bold tracking-wide">
+    <div className="cart flex h-full max-h-[calc(100vh_-_90px)] flex-col">
+      <h2 className="bg-blue-600 py-4 text-center font-bold tracking-wide text-white">
         Sepetteki Ürünler
       </h2>
-      <div className="cart-items">
-        <div className="cart-item">cart item</div>
-      </div>
+
+      <ul className="cart-items flex flex-col gap-y-3 overflow-y-auto px-2 py-2">
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img
+              src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg"
+              alt=""
+              className="h-16 w-16 object-cover"
+            />
+            <div className="ml-2 flex flex-col">
+              <b>Elma</b>
+              <span>12₺ x 2</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <Button
+              type="primary"
+              size="small"
+              className="flex w-full items-center justify-center !rounded-full"
+              icon={<PlusCircleOutlined />}
+            />
+            <span className="">1</span>
+            <Button
+              type="primary"
+              size="small"
+              className="flex w-full items-center justify-center !rounded-full"
+              icon={<MinusCircleOutlined />}
+            />
+          </div>
+        </li>
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img
+              src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg"
+              alt=""
+              className="h-16 w-16 object-cover"
+            />
+            <div className="ml-2 flex flex-col">
+              <b>Elma</b>
+              <span>12₺ x 2</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <Button
+              type="primary"
+              size="small"
+              className="flex w-full items-center justify-center !rounded-full"
+              icon={<PlusCircleOutlined />}
+            />
+            <span className="">1</span>
+            <Button
+              type="primary"
+              size="small"
+              className="flex w-full items-center justify-center !rounded-full"
+              icon={<MinusCircleOutlined />}
+            />
+          </div>
+        </li>
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img
+              src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg"
+              alt=""
+              className="h-16 w-16 object-cover"
+            />
+            <div className="ml-2 flex flex-col">
+              <b>Elma</b>
+              <span>12₺ x 2</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <Button
+              type="primary"
+              size="small"
+              className="flex w-full items-center justify-center !rounded-full"
+              icon={<PlusCircleOutlined />}
+            />
+            <span className="">1</span>
+            <Button
+              type="primary"
+              size="small"
+              className="flex w-full items-center justify-center !rounded-full"
+              icon={<MinusCircleOutlined />}
+            />
+          </div>
+        </li>
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img
+              src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg"
+              alt=""
+              className="h-16 w-16 object-cover"
+            />
+            <div className="ml-2 flex flex-col">
+              <b>Elma</b>
+              <span>12₺ x 2</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <Button
+              type="primary"
+              size="small"
+              className="flex w-full items-center justify-center !rounded-full"
+              icon={<PlusCircleOutlined />}
+            />
+            <span className="">1</span>
+            <Button
+              type="primary"
+              size="small"
+              className="flex w-full items-center justify-center !rounded-full"
+              icon={<MinusCircleOutlined />}
+            />
+          </div>
+        </li>
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img
+              src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg"
+              alt=""
+              className="h-16 w-16 object-cover"
+            />
+            <div className="ml-2 flex flex-col">
+              <b>Elma</b>
+              <span>12₺ x 2</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <Button
+              type="primary"
+              size="small"
+              className="flex w-full items-center justify-center !rounded-full"
+              icon={<PlusCircleOutlined />}
+            />
+            <span className="">1</span>
+            <Button
+              type="primary"
+              size="small"
+              className="flex w-full items-center justify-center !rounded-full"
+              icon={<MinusCircleOutlined />}
+            />
+          </div>
+        </li>
+      </ul>
+
       <div className="cart-totals mt-auto">
         <div className="border-t border-b">
           <div className="flex justify-between p-2">
@@ -21,7 +166,7 @@ const CartTotals = () => {
             <span className="text-red-700">+7.92₺</span>
           </div>
         </div>
-        <div className="border-b mt-4">
+        <div className="mt-4 border-b">
           <div className="flex justify-between p-2">
             <b className="text-xl text-green-500">Genel Toplam</b>
             <span className="text-xl">99₺</span>
@@ -34,7 +179,7 @@ const CartTotals = () => {
           <Button
             type="primary"
             size="large"
-            className="w-full mt-2 flex items-center justify-center"
+            className="mt-2 flex w-full items-center justify-center"
             icon={<ClearOutlined />}
             danger
           >
