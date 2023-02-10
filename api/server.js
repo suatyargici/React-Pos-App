5bbb848c275b48e1d7f6597ed890425c058dec1e
Suat-17 <yargicisuat@gmail.com>
2023-02-10 22:41:44 +0300

    feat: added mongoose

--- api/server.js
+++ api/server.js
@@ -1,8 +1,22 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
 const port = 5000;
 
+dotenv.config(); 
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/", (req,res)=> res.send("Hello World!"))
 
 app.listen(port, () => {
