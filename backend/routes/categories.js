d4afd0a9ccad75e017da106368ef1c784f930a1a
Suat-17 <yargicisuat@gmail.com>
2023-03-26 16:24:28 +0300

    chore: added routes

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -2,6 +2,15 @@ const Category = require("../models/Category.js");
 const express = require("express");
 const router = express.Router();
 
+router.get("/get-all", async(req,res) =>{
+    try{
+        const categories = await Category.find()
+        res.status(200).json(categories)
+    }catch(error){
+        res.status(400).json(error)
+    }
+})
+
 router.post("/add-category", async (req, res) => {
   try {
     const newCategory = new Category(req.body);
@@ -11,6 +20,25 @@ router.post("/add-category", async (req, res) => {
     res.status(400).json(error);
   }
 });
+
+router.put("/update-category", async(req,res) =>{
+  try{
+    await Category.findOneAndUpdate({_id:req.body._id},req.body)
+      res.status(200).json("Item updated successfully.")
+  }catch(error){
+      res.status(400).json(error)
+  }
+})
+
+
+router.delete("/delete-category", async(req,res) =>{
+  try{
+    await Category.findOneAndDelete({_id:req.body._id})
+      res.status(200).json("Item delete successfully.")
+  }catch(error){
+      res.status(400).json(error)
+  }
+})
 // router.post("/get-all", async (req, res) => {
 //   try {
 //     const newCategory = await Category.find()
