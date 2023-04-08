8d70ac8524ed163286d92cd98ff1dacf8ab4b6f5
Suat-17 <yargicisuat@gmail.com>
2023-04-09 01:01:33 +0300

    chore: added productItem component

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -1,37 +1,19 @@
 import axios from "axios";
 import { useQuery } from "react-query";
+import ProductItem from "./ProductItem";
 
 const Products = () => {
+  const getProducts = () => {
+    return axios.get(`http://localhost:5000/api/products/get-all`);
+  };
 
-  const getProducts =()=>{
-    return axios.get(`http://localhost:5000/api/products/get-all`)
-}
-  
-  const {data} = useQuery("product",getProducts)
+  const { data } = useQuery("product", getProducts);
 
   return (
     <div className="products-wrapper grid grid-cols-card gap-4">
-      {
-        data?.data.map((item)=>(
-
-          <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none" key={item.id}>
-          <div className="product-img">
-            <img
-              src={item?.img}
-              alt=""
-              className="h-28 object-cover w-full border-b"
-            />
-          </div>
-          <div className="product-info flex flex-col p-3">
-            <span className="font-bold">{item?.title}</span>
-            <span>{item?.price}₺</span>
-          </div>
-        </div>
-        )
-        )
-      }
-
-      
+      {data?.data.map((item) => (
+        <ProductItem key={item.id} item={item}/>
+      ))}
     </div>
   );
 };
