6a1038d81d786b34bc8c2548802ff74832ff9a52
Suat-17 <yargicisuat@gmail.com>
2023-04-16 01:51:40 +0300

    chore: ı fixed the statistic page

--- client/src/pages/CustomerPage.jsx
+++ client/src/pages/CustomerPage.jsx
@@ -1,8 +1,17 @@
 import { Table } from "antd";
 import { useTranslation, initReactI18next } from "react-i18next";
 import Header from "../components/header/Header.jsx";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 const CustomerPage = () => {
+  const getBills = () => {
+    return axios.get("http://localhost:5000/api/bills/get-all");
+  };
+
+  const { data } = useQuery("bills", getBills);
+
+
   const dataSource = [
     {
       key: "1",
@@ -17,36 +26,41 @@ const CustomerPage = () => {
       address: "10 Downing Street",
     },
   ];
-
   const columns = [
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
+      title: "Müşteri Adı",
+      dataIndex: "customerName",
+      key: "customerName",
     },
     {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
+      title: "Telefon Numarası",
+      dataIndex: "customerPhoneNumber",
+      key: "customerPhoneNumber",
     },
     {
-      title: "Address",
-      dataIndex: "address",
-      key: "address",
+      title: "İşlem Tarihi",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (text) => {
+        return <span>{text.substring(0, 10)}</span>;
+      },
     },
   ];
 
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <>
       <Header />
       <div className="px-6">
-        <h1 className="text-4xl font-bold text-center mb-4">{t("my-customers")}</h1>
+        <h1 className="mb-4 text-center text-4xl font-bold">
+          {t("my-customers")}
+        </h1>
         <Table
-          dataSource={dataSource}
+          dataSource={data?.data}
           columns={columns}
           bordered
+          rowKey={"_id"}
           pagination={false}
         />
       </div>
--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -1,57 +1,30 @@
+import axios from "axios";
 import Header from "../components/header/Header.jsx";
 import StatisticCard from "../components/statistics/StatisticsCard.jsx";
 import { Area, Pie } from "@ant-design/plots";
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 
 const StatisticPage = () => {
-  const [data, setData] = useState([]);
 
-  useEffect(() => {
-    asyncFetch();
-  }, []);
 
-  const asyncFetch = () => {
-    fetch(
-      "https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json"
-    )
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => {
-        console.log("fetch data failed", error);
-      });
+  const getProducts = () => {
+    return axios.get(`http://localhost:5000/api/products/get-all`);
   };
 
-  const data2 = [
-    {
-      type: '分类一',
-      value: 27,
-    },
-    {
-      type: '分类二',
-      value: 25,
-    },
-    {
-      type: '分类三',
-      value: 18,
-    },
-    {
-      type: '分类四',
-      value: 15,
-    },
-    {
-      type: '分类五',
-      value: 10,
-    },
-    {
-      type: '其他',
-      value: 5,
-    },
-  ];
+  const { data:products } = useQuery("product", getProducts);
+
+  const getBills = () => {
+    return axios.get("http://localhost:5000/api/bills/get-all");
+  };
+
+  const { data } = useQuery("bills", getBills);
+  console.log('data :>> ', data);
+
 
   const config = {
-    data,
-    xField: "timePeriod",
-    yField: "value",
+    data:data?.data,
+    xField: "customerName",
+    yField: "subTotal",
     xAxis: {
       range: [0, 1],
     },
@@ -59,9 +32,9 @@ const StatisticPage = () => {
 
   const config2 = {
     appendPadding: 10,
-    data: data2,
-    angleField: "value",
-    colorField: "type",
+    data:data?.data,
+    angleField: "subTotal",
+    colorField: "customerName",
     radius: 1,
     innerRadius: 0.6,
     label: {
@@ -89,13 +62,40 @@ const StatisticPage = () => {
           overflow: "hidden",
           textOverflow: "ellipsis",
         },
-        content: "AntV\nG2Plot",
+        content: "Toplam\nDeğer",
       },
     },
+  };
+
+  const totalAmount = () => {
+    const amount = data?.data.reduce((total, item) => item.totalAmount + total, 0);
+    return `${amount.toFixed(2)}₺`;
   };
   return (
     <>
       <Header />
+      <div className="statistic-cards grid xl:grid-cols-4 md:grid-cols-2 my-10 md:gap-10 gap-4">
+            <StatisticCard
+              title={"Toplam Müşteri"}
+              amount={data?.data?.length}
+              img={"images/user.png"}
+            />
+            <StatisticCard
+              title={"Toplam Kazanç"}
+              amount={totalAmount()}
+              img={"images/money.png"}
+            />
+            <StatisticCard
+              title={"Toplam Satış"}
+              amount={data?.data.length}
+              img={"images/sale.png"}
+            />
+            <StatisticCard
+              title={"Toplam Ürün"}
+              amount={products?.data?.length}
+              img={"images/product.png"}
+            />
+          </div>
       <div className="px-6 md:pb-0 pb-20">
         <h1 className="text-4xl font-bold text-center mb-4">İstatistiklerim</h1>
         <div className="statistic-section">
